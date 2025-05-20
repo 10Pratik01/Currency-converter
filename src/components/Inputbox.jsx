f416cbeec70da127defc9da550afa36905f013bd
10Pratik01 <using0this@gmail.com>
2025-05-20 19:47:14 +0530

    completed Inputbox

--- src/components/Inputbox.jsx
+++ src/components/Inputbox.jsx
@@ -5,7 +5,7 @@ function Inputbox({
   amount,
   changeAmount,
   changeCurrency,
-  currencyOption = [],
+  currencyOptions = [],
   selectCurrency = "inr",
   amountdisable = false,
   currencydisable = false,
@@ -21,12 +21,24 @@ function Inputbox({
             py-1.5"
           type="number"
           placeholder="Amount"
+          disabled={amountdisable}
+          value={amount}
+          onChange={(e) => changeAmount && changeAmount(Number(e.target.value))}
         />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
-        <select className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none">
-          <option value="inr">inr</option>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectCurrency}
+          onChange={(e) => changeCurrency && changeCurrency(e.target.value)}
+          disabled={currencydisable}
+        >
+            {currencyOptions.map((currency)=>(
+                <option key={currency} value={currency}>
+                    {currency}
+                </option>
+            ) )}
         </select>
       </div>
     </div>
