28b2302abccd7d0d158c7aebb2f28caeaa1f3027
10Pratik01 <using0this@gmail.com>
2025-05-21 08:14:35 +0530

    Almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import React, {useState}from 'react'; 
 import {Inputbox} from './components';
-import useCurrency from './hooks/useCurrencyInfos'
+import useCurrency from './hooks/useCurrencyInfo'
 
 function App() { 
   const [amount, setAmount] = useState(0)
   const [from, setFrom] = useState('inr')
   const [to, setTo] = useState('usd')
+  const [result, setResult] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from) 
+
+  const options = Object.keys(currencyInfo)
+
+  // to swap two variables 
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setResult(amount)
+    setAmount(result)
+  }
+
+  const convert = () => {
+    setResult(amount * currencyInfo[to].value) 
+  }
+
   return (
     <div className=''>
         <>
-          <h1 className='text-3xl'>Currency converter app</h1>
+          <h1 className='text-3xl bg-orange-500'>Currency converter app</h1>
         </>
     </div>
   );
